ce9cbcceedd127902edd65adbdb284b5528dd299
Namit Chandwani <namitchandwani99@gmail.com>
2020-12-24 07:22:27 +0530

    Set up GraphQL in App module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { NoteModule } from './note/note.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    NoteModule,
+  ],
   controllers: [],
   providers: [],
 })

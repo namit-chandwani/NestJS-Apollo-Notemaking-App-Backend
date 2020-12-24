95d0d05db3b05f3af47c082c01a1f8b8ab860e3f
Namit Chandwani <namitchandwani99@gmail.com>
2020-12-24 07:25:09 +0530

    Set up TypeORM for MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { NoteModule } from './note/note.module';
+import { Note } from './note/note.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/todo',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Note],
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
